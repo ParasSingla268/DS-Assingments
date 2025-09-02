4609c58d257cbf14c3f47d9bbbcb1e568df717e6
Paras Singla <parassingla268@gmail.com>
2025-09-02 13:26:15 +0530

    Add files via upload

---
+++ Assignment_3/Question_2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    cout << "Enter string: ";
+    getline(cin, s);
+
+    char arr[1000];
+    int top = -1;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        arr[++top] = s[i];
+    }
+
+    string rev = "";
+
+    while (top >= 0)
+    {
+        rev += arr[top--];
+    }
+
+    cout << "Reversed string: " << rev << "\n";
+    return 0;
+}
